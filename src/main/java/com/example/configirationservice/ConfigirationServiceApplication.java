e4f422637485bea3446a25f7cdcd236f6cf97cf9
Asatbek Xalimjonov <asatbek.xalimjonov@dohatec.net>
2022-11-10 10:27:16 +0500

    first commit

---
+++ src/main/java/com/example/configirationservice/ConfigirationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.configirationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigirationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigirationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/configirationservice/ConfigirationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configirationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigirationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
